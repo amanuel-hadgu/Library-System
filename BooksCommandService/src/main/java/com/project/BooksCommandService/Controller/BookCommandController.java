97fafd22d15043f8b2fa5d2b3f52c55626a0d28e
Abiexx <abiel.berhane20@gmail.com>
2023-01-21 23:34:15 -0500

    trying

--- BooksCommandService/src/main/java/com/project/BooksCommandService/Controller/BookCommandController.java
+++ BooksCommandService/src/main/java/com/project/BooksCommandService/Controller/BookCommandController.java
@@ -25,6 +25,7 @@ public class BookCommandController {
         return new ResponseEntity<>( bookDto, HttpStatus.OK);
     }
 
+    //aaaa
     @DeleteMapping("/delete")
     public ResponseEntity<?> deleteBook(@PathVariable long isbn){
        BookDto bookDto = iBookCommandService.deleteBook(isbn);
