be73cb0195223a78bedce766e8b2d4434b2e38fd
MICHAEL KINFE <akikinfe@gmail.com>
2023-01-21 23:18:32 -0500

    creating controller

--- ReviewService/src/main/java/com/project/ReviewService/Dto/ReviewDto.java
+++ ReviewService/src/main/java/com/project/ReviewService/Dto/ReviewDto.java
@@ -2,5 +2,9 @@ package com.project.ReviewService.Dto;
 
 public class ReviewDto {
 
+ private String description;
+ private String isbn;
+ private Integer rating;
+ private String customerNumber;
 
 }
--- ReviewService/src/main/java/com/project/ReviewService/controller/ReviewController.java
+++ ReviewService/src/main/java/com/project/ReviewService/controller/ReviewController.java
@@ -1,4 +1,21 @@
 package com.project.ReviewService.controller;
 
+import com.project.ReviewService.Dto.ReviewDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reviews")
 public class ReviewController {
+
+    @PostMapping
+    public ResponseEntity<?> createReview(@RequestBody ReviewDto reviewDto){
+        return null;
+    }
+
+
+    //to try
 }
