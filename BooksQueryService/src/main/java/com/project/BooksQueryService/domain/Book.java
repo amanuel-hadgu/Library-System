fdaf308202f00f0b6156628f2b3e66e99964b0ee
amanuel <emahadgugm@gmail.com>
2023-01-22 15:56:45 -0600

    @Document annotation added

--- BooksQueryService/src/main/java/com/project/BooksQueryService/domain/Book.java
+++ BooksQueryService/src/main/java/com/project/BooksQueryService/domain/Book.java
@@ -1,12 +1,14 @@
 package com.project.BooksQueryService.domain;
 
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
+@Document
 public class Book {
     private String isbn;
     private String title;
--- BooksQueryService/src/main/java/com/project/BooksQueryService/domain/Review.java
+++ BooksQueryService/src/main/java/com/project/BooksQueryService/domain/Review.java
@@ -1,12 +1,14 @@
 package com.project.BooksQueryService.domain;
 
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
+@Document
 public class Review {
     private String isbn;
     private int rating;
