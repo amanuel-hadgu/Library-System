17a7763fae5e0c670ce2c6f939581ea4aab91eb9
DawitZerom <109552726+DawitZerom@users.noreply.github.com>
2023-01-23 02:33:31 -0600

    handle book change

--- BooksQueryService/src/main/java/com/project/BooksQueryService/BooksQueryServiceApplication.java
+++ BooksQueryService/src/main/java/com/project/BooksQueryService/BooksQueryServiceApplication.java
@@ -2,8 +2,10 @@ package com.project.BooksQueryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class BooksQueryServiceApplication {
 
 	public static void main(String[] args) {
