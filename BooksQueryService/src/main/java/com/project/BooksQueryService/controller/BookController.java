e2e37040f6b525ed30491e0939be70071082f863
DawitZerom <109552726+DawitZerom@users.noreply.github.com>
2023-01-27 20:09:35 -0600

    new

--- BooksQueryService/src/main/java/com/project/BooksQueryService/controller/BookController.java
+++ BooksQueryService/src/main/java/com/project/BooksQueryService/controller/BookController.java
@@ -1,4 +1,21 @@
 package com.project.BooksQueryService.controller;
 
+import com.project.BooksQueryService.service.dto.BookDTO;
+import com.project.BooksQueryService.service.impl.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/books/{book_id}")
+    public ResponseEntity<?> get(@PathVariable String book_id){
+        BookDTO bookDto = bookService.get(book_id);
+        return new ResponseEntity<>(bookDto, HttpStatus.OK);
+    }
 }
